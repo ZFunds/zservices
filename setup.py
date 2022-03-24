eacabb80a9acfa83f2cf52691fbe972a6044cf9e
Yogesh <yogintegrate@gmail.com>
2022-03-24 10:28:38 +0530

    0.1.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='zservices',
     packages=['zdynamodb'],
     package_dir={'zdynamodb': 'zdynamodb'},
-    version='0.1.6',
+    version='0.1.9',
     license='MIT',
     platforms='cross-platfom, platform-independent',
     description='ZFunds basic services',
