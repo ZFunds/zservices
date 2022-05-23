7794bf2fc341a712eee4b3eb4fe1f0ca35a244d1
Yogesh <yogintegrate@gmail.com>
2022-05-23 12:28:25 +0530

    zs3 upload streams and zsftp service

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='zservices',
-    packages=['zdynamodb', 'zdiscord', 'zrequests', 'zs3'],
-    package_dir={'zdynamodb': 'zdynamodb', 'zdiscord': 'zdiscord', 'zrequests': 'zrequests', 'zs3': 'zs3'},
-    version='0.2.3',
+    packages=['zdynamodb', 'zdiscord', 'zrequests', 'zs3', 'zsftp'],
+    package_dir={'zdynamodb': 'zdynamodb', 'zdiscord': 'zdiscord', 'zrequests': 'zrequests', 'zs3': 'zs3',
+                 'zsftp': 'zsftp'},
+    version='0.2.4',
     license='MIT',
     platforms='cross-platfom, platform-independent',
     description='ZFunds basic services',
@@ -13,7 +14,7 @@ setup(
     author_email='yogesh@zfunds.in',
     url='https://github.com/ZFunds/zservices/',
     download_url='https://github.com/ZFunds/zservices/',
-    keywords=['dynamodb', 'discord', 's3', 'requests'],
+    keywords=['dynamodb', 'discord', 's3', 'requests', 'sftp'],
     install_requires=[
         'python-dotenv==0.19.2', 'boto3==1.21.17', 'requests==2.27.1', 'paramiko==2.10.4'
     ],
