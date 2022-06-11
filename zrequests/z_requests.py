5c7e8b7a2242ea87defb190b8e77d7bd364eb41e
Yogesh <yogintegrate@gmail.com>
2022-06-11 14:23:47 +0530

    retry in error logs also

--- zrequests/z_requests.py
+++ zrequests/z_requests.py
@@ -30,14 +30,26 @@ def send_request(method, url, s_codes: list, params, headers, tag='N', json=None
                     return True, response
                 return verify_success_text(s_text, response)
             else:
-                logger.error(f'[ZREQUEST] [{tag}] {url}, "HTTP code not in s_codes", r={response},', exc_info=True)
-                return False, response
+                logger.warning(f'[ZREQUEST] [{tag}][{count}] {url}, e=HTTP code not in s_codes, r={response}')
+                count += 1
+                if sleep:
+                    time.sleep(2 ** count)
+                return send_request(method, url, s_codes, params, headers, tag, json, data, count, sleep, s_text)
+
         except Exception as e:
-            logger.warning(f'[ZREQUEST] [{tag}][{count}]. {url}, e={e}, r={response}', exc_info=True)
+            logger.warning(f'[ZREQUEST] [{tag}][{count}] {url}, e={e}, r={response}', exc_info=True)
             count += 1
             if sleep:
                 time.sleep(2 ** count)
             return send_request(method, url, s_codes, params, headers, tag, json, data, count, sleep, s_text)
 
-    logger.error(f'[ZREQUEST] [{tag}] {url}, r={response},', exc_info=True)
+    logger.error(f'[ZREQUEST] [{tag}] Failed to get proper response for {url} r={response},', exc_info=True)
     return False, response
+
+
+def handle_error_exception(method, url, s_codes, params, headers, tag, json, data, count, sleep, s_text):
+    logger.warning(f'[ZREQUEST] [{tag}][{count}]. {url}, e={e}, r={response}', exc_info=True)
+    count += 1
+    if sleep:
+        time.sleep(2 ** count)
+
