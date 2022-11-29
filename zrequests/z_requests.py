640c39d915ef466d182d7d1815eb371f24a1e09c
vibhav-zf <vibhav@zfunds.in>
2022-11-29 12:50:45 +0530

    moved retry to failure

--- zrequests/z_requests.py
+++ zrequests/z_requests.py
@@ -28,26 +28,26 @@ def verify_success_text(s_text, response: sync_req.Response):
 def send_request(method, url, s_codes: list, params, headers, tag='N', json=None, data=None, count=0, sleep=False,
                  s_text=None):
     response = None
-    if count < Setting.MAX_RETRY:
-        try:
-            response = sync_req.request(method, url=url, params=params, data=data, headers=headers, json=json)
-            if response.status_code in s_codes:
-                if not s_text:
-                    return True, response
-                return verify_success_text(s_text, response)
-            else:
-                logger.warning(f'[ZREQUEST] [{tag}][{count}] {url}, e=HTTP code not in s_codes, r={response}')
-                count += 1
-                if sleep:
-                    time.sleep(2 ** count)
-                return send_request(method, url, s_codes, params, headers, tag, json, data, count, sleep, s_text)
-
-        except Exception as e:
-            logger.warning(f'[ZREQUEST] [{tag}][{count}] {url}, e={e}, r={response}', exc_info=True)
+    try:
+        response = sync_req.request(method, url=url, params=params, data=data, headers=headers, json=json)
+        if response.status_code in s_codes:
+            if not s_text:
+                return True, response
+            return verify_success_text(s_text, response)
+        else:
+            logger.warning(f'[ZREQUEST] [{tag}][{count}] {url}, e=HTTP code not in s_codes, r={response}')
             count += 1
             if sleep:
                 time.sleep(2 ** count)
-            return send_request(method, url, s_codes, params, headers, tag, json, data, count, sleep, s_text)
+            if count < Setting.MAX_RETRY:
+                return send_request(method, url, s_codes, params, headers, tag, json, data, count, sleep, s_text)
 
-    logger.error(f'[ZREQUEST] [{tag}] Failed to get proper response for {url} r={response},', exc_info=True)
-    return False, response
+            logger.error(f'[ZREQUEST] [{tag}] Failed to get proper response for {url} r={response},', exc_info=True)
+            return False, response
+
+    except Exception as e:
+        logger.warning(f'[ZREQUEST] [{tag}][{count}] {url}, e={e}, r={response}', exc_info=True)
+        count += 1
+        if sleep:
+            time.sleep(2 ** count)
+        return send_request(method, url, s_codes, params, headers, tag, json, data, count, sleep, s_text)
